8f1cde0b63dceb127d1d81144e52a9b257252bde
denovanrr <137625696+denovanrr@users.noreply.github.com>
2023-09-24 09:22:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function HandleClick(event){
+
+    document.querySelector(".screen1").classList.add("hide")
+
+    document.querySelector(".screen2").classList.remove("hide")
+
+    
+
+}
+function Restart(){
+location.reload()
+
+}
